e7bd9d9a3685fc18e665a2521f41ebc7e059b1ad
mariano grimaux <mariano.grimaux@gmail.com>
2017-05-27 17:34:46 -0300

    change stack interface.

--- src/Programacion2/api/Stack.java
+++ src/Programacion2/api/Stack.java
@@ -2,17 +2,13 @@ package Programacion2.api;
 
 public interface Stack {
 
-    void push(int element);
-
     void initialize();
 
-    int pop();
+    void push(int element);
+
+    void pop();
 
     boolean isEmpty();
 
     int peek();
-
-    int size();
-
-    void print();
 }
