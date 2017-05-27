0e443d8498a7d5df0a3a1e451b36c74aeb6afbf0
mariano grimaux <mariano.grimaux@gmail.com>
2017-05-27 19:19:40 -0300

    Ejercicio 2c

--- src/Programacion2/ejercicios/tp1/Ejercicio2C.java
+++ src/Programacion2/ejercicios/tp1/Ejercicio2C.java
@@ -5,28 +5,28 @@ import Programacion2.api.Stack;
 import Programacion2.impl.StackImpl;
 
 //Invertir el contenido de una Pila.
-public class porterminarEjercicio2C {
+public class Ejercicio2C {
 
     public static void main(String[] args) {
         Stack stack = new StackImpl();
+        stack.initialize();
         Utils.fillStack(stack, 5);
 
         System.out.println("Los valores de la pila al principio del proceso son:");
-        stack.print();
+        Utils.printStaticStack(stack);
 
-        invertStack(stack, 0, stack.size());
+         invertStack(stack);
 
         System.out.println("\n\nLos valores de la pila al final del proceson son:");
-        stack.print();
+        Utils.printStaticStack(stack);
     }
 
-    private static void invertStack(Stack stack, int index, int stackOriginalSize){
-        if (index < stackOriginalSize) {
-            index++;
-            int pop = stack.pop();
-            invertStack(stack, index, stackOriginalSize);
-            stack.push(pop);
+    private static void invertStack(Stack stack) {
+        int peek = stack.peek();
+        stack.pop();
+        if (!stack.isEmpty()) {
+            invertStack(stack);
         }
-
+        stack.push(peek);
     }
 }
